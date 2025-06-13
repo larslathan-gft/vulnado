56f15bd41774652adec0b80a3f25ff9d9419c582
Lars Lathan <65671436+larslathan-gft@users.noreply.github.com>
2025-06-13 13:59:03 +0200

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -2,13 +2,19 @@ package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+  }
+    throw new IllegalStateException("Utility class");
+  private Cowsay() {
+  private static final Logger LOGGER = Logger.getLogger(Cowsay.class.getName());
 public class Cowsay {
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
     String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
+    LOGGER.log(Level.INFO, cmd);
+    processBuilder.command("bash", "-c", "/usr/games/cowsay", input);
 
     StringBuilder output = new StringBuilder();
 
@@ -21,8 +27,8 @@ public class Cowsay {
         output.append(line + "\n");
       }
     } catch (Exception e) {
-      e.printStackTrace();
+      LOGGER.log(Level.SEVERE, "An error occurred while executing the command", e);
     }
     return output.toString();
-  }
+    return output.toString();
 }
